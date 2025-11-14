736c2132c34145d022c3a64a0bbee5ad6923c237
Pravin-b49 <pravinbarewar4@gmail.com>
2025-11-14 20:55:35 +0530

    Updated Code

--- app.js
+++ app.js
@@ -5,28 +5,57 @@ const fs = require("fs");
 
 app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) =>{
-    fs.readdir(`./file`, function(err, files){
-        res.render("index", {files: files});
-    })
+app.get('/', (req, res) => {
+    fs.readdir("./file", (err, files) => {
+        if (err) return res.send("Error reading folder");
+        res.render("index", { files });
+    });
 });
 
-app.get('/file/:filename', (req, res) =>{
-    fs.readFile(`./file/${req.params.filename}`, "utf-8", function(err, filedata){
-        res.render('show', {filename: req.params.filename, filedata: filedata});
-    })
+app.get('/file/:filename', (req, res) => {
+    fs.readFile(`./file/${req.params.filename}`, "utf-8", (err, filedata) => {
+        if (err) return res.send("File not found");
+        res.render("show", { filename: req.params.filename, filedata });
+    });
 });
 
-app.post('/create', (req, res) =>{
-    // console.log(req.body);
-    fs.writeFile(`./file/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
+app.get('/edit/:filename', (req, res) => {
+    res.render('edit', { filename: req.params.filename });
+});
+
+app.get('/delete/:filename', (req, res) => {
+    res.render('delete', { filename: req.params.filename });
+});
+
+app.post('/delete/:filename', (req, res) => {
+    const file = req.params.filename;
+    fs.unlink(`./file/${file}`, (err) => {
+        if (err) {
+            console.error("Error deleting file:", err);
+        }
+        console.log(`${file}`, "File Deleted Successfully.!");
+        res.redirect("/");
+    });
+});
+
+app.post('/edit', (req, res) => {
+    fs.rename(`./file/${req.body.previous}`, `./file/${req.body.new}`, (err) => {
+        if (err) console.log(err);
+        res.redirect("/");
+    });
+});
+
+app.post('/create', (req, res) => {
+    const name = req.body.title.split(' ').join('') + ".txt";
+    fs.writeFile(`./file/${name}`, req.body.details, (err) => {
+        if (err) console.log(err);
         res.redirect('/');
-    })
+    });
 });
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log("Server is running on Port 3000");
-})
+});
